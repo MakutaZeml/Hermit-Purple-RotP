ca61244ef4302983d0ed4376c2f221d8022f64dd
Makuta Zeml <152751407+MakutaZeml@users.noreply.github.com>
2024-06-05 21:11:41 -0600

    Fixed respiration bug

--- src/main/java/com/zeml/rotp_zhp/action/stand/HamonBreath.java
+++ src/main/java/com/zeml/rotp_zhp/action/stand/HamonBreath.java
@@ -7,19 +7,22 @@ import com.github.standobyte.jojo.entity.stand.StandEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.init.ModSounds;
 import com.github.standobyte.jojo.init.power.non_stand.ModPowers;
+import com.github.standobyte.jojo.init.power.non_stand.hamon.ModHamonActions;
+import com.github.standobyte.jojo.network.packets.fromserver.HamonSyncOnLoadPacket;
 import com.github.standobyte.jojo.network.packets.fromserver.TrHamonEnergyTicksPacket;
 import com.github.standobyte.jojo.power.impl.nonstand.INonStandPower;
 import com.github.standobyte.jojo.power.impl.nonstand.type.hamon.HamonData;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.zeml.rotp_zhp.entity.stand.stands.HermitPurpleEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.EntityPredicates;
 import net.minecraft.world.World;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class HamonBreath extends StandEntityAction {
-
     public HamonBreath(StandEntityAction.Builder builder){
         super(builder);
     }
@@ -54,19 +57,47 @@ public class HamonBreath extends StandEntityAction {
                  ipower.getTypeSpecificData(ModPowers.HAMON.get()).map(
                         hamonData -> {
                              tics.set(hamonData.getBreathingLevel()*hamonData.getMaxBreathStability() / hamonData.getMaxEnergy());
+
                             return null;
+
                         }
                 );
-                 ipower.addEnergy(120+tics.get());
-
+                 ipower.addEnergy(10+tics.get());
+                 ipower.setHeldAction(ModHamonActions.HAMON_BREATH.get());
             });
         }
 
     }
 
+/*
+    @Override
+    public void stoppedHolding(World world, LivingEntity user, IStandPower power, int ticksHeld, boolean willFire) {
+        if(getHermitPurple(user).isPresent()){
+            INonStandPower.getNonStandPowerOptional(user).ifPresent(ipower->{
+                getHermitPurple(user).get().hamon(ticksHeld);
+                ipower.getTypeSpecificData(ModPowers.HAMON.get()).map(
+                        hamonData -> {
+                            return null;
+                        }
+                );
+
+            });
+        }
+        invokeForStand(power, StandEntity::stopTaskWithRecovery);
+    }
+
+ */
+
     @Override
     public boolean enabledInHudDefault() {
         return false;
+    }
+
+
+
+    public Optional<HermitPurpleEntity> getHermitPurple(LivingEntity player){
+        return  player.level.getEntitiesOfClass(HermitPurpleEntity.class, player.getBoundingBox().inflate(3), EntityPredicates.ENTITY_STILL_ALIVE).stream()
+                .filter(entity -> (entity.getUser() == player)).findAny();
     }
 
 
